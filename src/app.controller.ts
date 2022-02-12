3812918325a5f59cd35056ac76d3f94651a413ba
Wirlley Delfino <wirlley_d@hotmail.com>
2022-02-12 16:37:28 -0300

    Adding healthz

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
+import {Response} from 'express'
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
+  private started_at = new Date()
   constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get("/healthz")
+  healthz(@Res() response: Response) {
+    const current_date = new Date();
+    const date_in_seconds = (current_date.getTime() - this.started_at.getTime()) / 1000;
+    if(date_in_seconds <= 30){
+      return response
+      .status(200)
+      .send("Im alive");
+    }
+    return response
+      .status(500)
+      .send("Im broken");
   }
 }
